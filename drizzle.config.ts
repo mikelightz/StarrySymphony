fd64935da0556448c01ba63792b92a154ba744f7
Michael Edwards <michael@macbookair.lan>
2025-05-22 23:29:12 -0400

    drizzleconfigts updt

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,19 +1,16 @@
 import { defineConfig } from "drizzle-kit";
-import dotenv from "dotenv";
-
-dotenv.config({
-  path: ".env.local",
-});
+import "dotenv/config";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
 
 export default defineConfig({
-  out: "./migrations",
+  out: "./drizzle",
   schema: "./shared/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
